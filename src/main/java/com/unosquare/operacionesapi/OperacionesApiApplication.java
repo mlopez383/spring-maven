06dfd060cafea3b134497e26e41f89112f09bec7
Miguel Lopez <mlopez383@gmail.com>
2023-10-16 16:13:04 -0400

    spring init

---
+++ src/main/java/com/unosquare/operacionesapi/OperacionesApiApplication.java
@@ -0,0 +1,13 @@
+package com.unosquare.operacionesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacionesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unosquare/operacionesapi/OperacionesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unosquare.operacionesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacionesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
